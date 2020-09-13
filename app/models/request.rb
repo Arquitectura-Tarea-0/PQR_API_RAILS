6d7419a24bc17c8bda4e3dc84032ee62cddad893
JoDaBaRo <josdabayona@gmail.com>
2020-09-12 23:02:19 -0500

    I'm done being dumb

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,9 +4,5 @@ class Request < ApplicationRecord
   belongs_to :user
   has_many :notes
   enum request_state: %i[settled in_progress solved closed]
-  before_create :default_state
 
-  def default_state
-  	self.request_state = 0
-  end
 end
--- db/migrate/20200913021329_create_requests.rb
+++ db/migrate/20200913021329_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
     	t.string :request_state
-    	t.string :request_type
+    	t.integer :request_type, default: 0
     	t.string :subject
     	t.text   :description
     	t.datetime :responded_at
