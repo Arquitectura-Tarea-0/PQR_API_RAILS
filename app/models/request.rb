c3699e804b82a9be3972dfabba7519b5513a747e
JoDaBaRo <josdabayona@gmail.com>
2020-09-12 22:54:40 -0500

    Extremely dumb

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,9 +4,9 @@ class Request < ApplicationRecord
   belongs_to :user
   has_many :notes
   enum request_state: %i[settled in_progress solved closed]
-  after_create :default_state
+  before_create :default_state
 
   def default_state
-  	self.update(request_state: 0)
+  	self.request_state = 0
   end
 end
