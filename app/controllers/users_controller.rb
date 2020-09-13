e21a8a1e27b0935ddf6785fcbab840306a71757b
cristianleonardop <cristianleonardo999@gmail.com>
2020-09-12 20:06:47 -0500

    refactor: use find by email instead find by username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   # LOGGING IN
   def login
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
       token = encode_token({user_id: @user.id})
