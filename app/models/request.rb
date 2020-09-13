48c4065f3643bec8fd825ff6b2b7c1dcf825ff56
JoDaBaRo <josdabayona@gmail.com>
2020-09-12 22:46:41 -0500

    adds static states to requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,9 @@
 class Request < ApplicationRecord
   belongs_to :user
   has_many :notes
+  enum request_state: %i[settled in_progress solved closed]
+
+  def default_state
+  	self.update(request_state: 0)
+  end
 end
